f9209f8a67f2a3861fc177da1acfcbbff0b6ab2a
LYMpehfrom05 <105274018+LYMpehfrom05@users.noreply.github.com>
2023-12-06 21:59:27 +0800

    Q2e

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/TestMatrix.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/TestMatrix.cs
@@ -12,6 +12,20 @@ public class TestMatrix : MonoBehaviour
     {
         mat.setIdentity();
         mat.Print();
+        Question2();
+    }
+
+    public Question2()
+    {
+        // Declare objects
+        HMatrix2D mat1 = new HMatrix2D();
+        HMatrix2D mat2 = new HMatrix2D();
+        HMatrix2D resultMat;
+        HVector2D vec1 = new HVector2D(1.0f, 3.0f);
+
+        // Assign resultMat to the value of mat1 and mat2 multiplied
+        resultMat = mat1 * mat2;
+
     }
 
 }
