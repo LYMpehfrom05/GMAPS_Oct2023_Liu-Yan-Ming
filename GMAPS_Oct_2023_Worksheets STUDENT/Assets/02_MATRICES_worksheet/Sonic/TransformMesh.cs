5a0daf212bbb8520f6568ac16aa313b02518073d
LYMpehfrom05 <105274018+LYMpehfrom05@users.noreply.github.com>
2023-12-07 06:29:47 +0800

    Q5d

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/TransformMesh.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/TransformMesh.cs
@@ -23,7 +23,7 @@ public class TransformMesh : MonoBehaviour
         meshManager = GetComponent<MeshManager>();
         pos = new HVector2D(gameObject.transform.position.x, gameObject.transform.position.y);
 
-        // Your code here
+        Translate(1,1);
     }
 
 
