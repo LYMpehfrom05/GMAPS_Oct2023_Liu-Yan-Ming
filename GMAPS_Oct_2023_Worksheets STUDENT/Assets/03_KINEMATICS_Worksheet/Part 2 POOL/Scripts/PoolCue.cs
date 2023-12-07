8ee5a79dd9d17635a2700aa7b17bc03ef17aba06
LYMpehfrom05 <105274018+LYMpehfrom05@users.noreply.github.com>
2023-12-07 19:33:05 +0800

    Part c done prior to this, Part d

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/PoolCue.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/PoolCue.cs
@@ -1,58 +1,61 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class PoolCue : MonoBehaviour
-// {
-// 	public LineFactory lineFactory;
-// 	public GameObject ballObject;
+public class PoolCue : MonoBehaviour
+{
+    public LineFactory lineFactory;
+    public GameObject ballObject;
 
-// 	private Line drawnLine;
-// 	private Ball2D ball;
+    private Line drawnLine;
+    private Ball2D ball;
 
-// 	private void Start()
-// 	{
-// 		ball = ballObject.GetComponent<Ball2D>();
-// 	}
+    private void Start()
+    {
+        ball = ballObject.GetComponent<Ball2D>();
+    }
 
-// 	void Update()
-// 	{
-// 		if (Input.GetMouseButtonDown(0))
-// 		{
-// 			var startLinePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Start line drawing
-// 			if (ball != null && ball.IsCollidingWith(/*your code here*/))
-// 			{
-// 				drawnLine = /*your code here*/;
-// 				drawnLine.EnableDrawing(true);
-// 			}
-// 		}
-// 		else if (Input.GetMouseButtonUp(0) && drawnLine != null)
-// 		{
-// 			drawnLine.EnableDrawing(false);
+    void Update()
+    {
+        // If left mouse button is clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Set start line by converting mouse position to world space
+            var startLinePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+            // If mouse collides with the existing ball, create and start drawing a line
+            if (ball != null && ball.IsCollidingWith(startLinePos))
+            {
+                drawnLine = lineFactory.CreateLine(startLinePos);
+                drawnLine.EnableDrawing(true);
+            }
+        }
+        else if (Input.GetMouseButtonUp(0) && drawnLine != null)
+        {
+            drawnLine.EnableDrawing(false);
 
-// 			//update the velocity of the white ball.
-// 			HVector2D v = new HVector2D(/*your code here*/);
-// 			ball./*your code here*/ = v;
+            //update the velocity of the white ball.
+            HVector2D v = new HVector2D(/*your code here*/);
+            ball./*your code here*/ = v;
 
-// 			drawnLine = null; // End line drawing            
-// 		}
+            drawnLine = null; // End line drawing            
+        }
+        // Update line end at mouse position while drawing
+        if (drawnLine != null)
+        {
+            drawnLine.end = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
 
-// 		if (drawnLine != null)
-// 		{
-// 			drawnLine.end = /*your code here*/; // Update line end
-// 		}
-// 	}
+    /// <summary>
+    /// Get a list of active lines and deactivates them.
+    /// </summary>
+    public void Clear()
+    {
+        var activeLines = lineFactory.GetActive();
 
-// 	/// <summary>
-// 	/// Get a list of active lines and deactivates them.
-// 	/// </summary>
-// 	public void Clear()
-// 	{
-// 		var activeLines = lineFactory.GetActive();
-
-// 		foreach (var line in activeLines)
-// 		{
-// 			line.gameObject.SetActive(false);
-// 		}
-// 	}
-// }
+        foreach (var line in activeLines)
+        {
+            line.gameObject.SetActive(false);
+        }
+    }
+}
