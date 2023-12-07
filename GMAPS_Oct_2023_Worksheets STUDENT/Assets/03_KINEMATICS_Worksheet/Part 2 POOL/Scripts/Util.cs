c76ec82c3672617fab732ab521b2f601d117aaeb
LYMpehfrom05 <105274018+LYMpehfrom05@users.noreply.github.com>
2023-12-07 14:52:56 +0800

    Game Imp 4 Part b

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/03_KINEMATICS_Worksheet/Part 2 POOL/Scripts/Util.cs
@@ -1,12 +1,17 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
 
-// public class Util
-// {
-//     public static float FindDistance(HVector2D p1, HVector2D p2)
-//     {
-//         return /*your code here*/;
-//     }
-// }
+ public class Util
+ {
+     public static float FindDistance(HVector2D p1, HVector2D p2)
+     {
+        // Difference in 2 x and y coordinates
+        float dx = p2.x - p1.x;
+        float dy = p2.y - p1.y;
+
+        // Distance is sqrt(dx^2 + dy^2) according to Pythagora's Theorem
+        return Mathf.Sqrt(dx * dx + dy * dy);
+    }
+ }
 
